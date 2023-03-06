cc2639869876896b00e2ffd8d019d01b8bbd421b
Quy15 <hungquy10101996@gmail.com>
2023-03-06 21:04:07 +0700

    upload

---
+++ QLCH/CuaHangDoChoi/CuaHangDoChoi/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CuaHangDoChoi.Models;
+
+namespace CuaHangDoChoi.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            QLCHEntities db = new QLCHEntities();
+            //Lấy ds sản phẩm
+
+            List<Product> pro = db.Products.ToList();
+            return View(pro);
+        }
+
+        public ActionResult Details(int id)
+        {
+            QLCHEntities db = new QLCHEntities();
+            Product details = db.Products.Find(id);
+            return View(details);
+        }
+
+
+    }
+}
